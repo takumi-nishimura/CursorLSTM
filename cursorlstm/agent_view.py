1ce356f794f1ec92b8d9e34764e7cb15ceb4ac10
takumi-nishimura <clp13218@nitech.jp>
2024-06-17 14:50:21 +0900

    add agent view

---
+++ cursorlstm/agent_view.py
@@ -0,0 +1,15 @@
+from gym_env import TaskEnvWrapper
+from stable_baselines3 import PPO
+
+model = PPO.load("model/cursor_agent")
+
+env = TaskEnvWrapper()
+
+obs = env.reset()
+done = False
+
+while not done:
+    action, _ = model.predict(obs, deterministic=True)
+    obs, reward, done, _ = env.step(action)
+    print(f"Action: {action}, Reward: {reward}")
+    env.render()
